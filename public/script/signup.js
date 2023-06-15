12c1ea35d9d6d4f414cd7115854a0e1e2f2457b0
James Ocao <98975725+csjames03@users.noreply.github.com>
2023-06-15 22:19:53 +0800

    Add files via upload
    Blood Bank Sign In User Interface finished

---
+++ public/script/signup.js
@@ -0,0 +1,7 @@
+ document.getElementById("button").addEventListener("click",function(){
+    document.getElementById('file-input').click();
+ })
+
+ document.getElementById("button").addEventListener("onChange", function(){
+    document.getElementById('val').text(this.value.replace(/C:\\fakepath\\/i, ''))
+ })
