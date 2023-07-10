ef7722a3e89681128ce6992650812600063184e1
James Ocao <98975725+csjames03@users.noreply.github.com>
2023-07-10 04:58:58 +0000

    Add files via upload

---
+++ adminconfig.php
@@ -0,0 +1,4 @@
+<?php
+
+
+$admin = array('admin' => 'James', 'password' => 'Confidential');
--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-require './views/Signup.html';
+session_start();
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+if ($url === '/bloodbank/') {
+    require 'controllers/main.controller.php';
+} else if ($url === '/bloodbank/logout') {
+    require 'partials/logout.php';
+} else if ($url === '/bloodbank/adminlogin') {
+    require 'controllers/adminlogin.controller.php';
+} else if ($url === '/bloodbank/requests') {
+    require 'controllers/requests.controller.php';
+} else if ($url === '/bloodbank/donations') {
+    require 'controllers/donation.controller.php';
+} else if ($url === '/bloodbank/stocks') {
+    require 'controllers/stocks.controller.php';
+} else {
+    require 'view/404.php';
+}
